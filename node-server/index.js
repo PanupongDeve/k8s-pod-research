bc84f1e01a6223e75c03d78776ba1aa528e3c87f
Panupong Chamsomboon <5635512110@psu.ac.th>
2021-04-10 08:02:33 +0000

    add app logic

--- node-server/index.js
+++ node-server/index.js
@@ -7,21 +7,45 @@ app.use(bodyParser.json())
 
 let respone_timeout = 200;
 
+let respone_error_timeout = 10000;
+
 
 app.get('/set-timeout/:timeout', (req, res) => {
     const { timeout } = req.params;
     respone_timeout = timeout;
     res.send({
-        respone_timeout: `${respone_timeout} ms`
+        respone_timeout: `${respone_timeout} ms`,
+        respone_error_timeout: `${respone_error_timeout} ms`
+    })
+})
+
+app.get('/set-error-timeout/:timeout', (req, res) => {
+    const { timeout } = req.params;
+    respone_error_timeout = timeout;
+    res.send({
+        respone_timeout: `${respone_timeout} ms`,
+        respone_error_timeout: `${respone_error_timeout} ms`
     })
 })
 
 app.get('/healthz', (req, res) => {
-    setTimeout(() => {
-        res.send({
-            respone_timeout: `${respone_timeout} ms`
-        })
-    }, respone_timeout) 
+
+    if (respone_timeout < respone_error_timeout) {
+        setTimeout(() => {
+            res.send({
+                respone_timeout: `${respone_timeout} ms`,
+                respone_error_timeout: `${respone_error_timeout} ms`
+            })
+        }, respone_timeout) 
+    } else {
+        setTimeout(() => {
+            res.status(500).send({
+                respone_timeout: `${respone_timeout} ms`,
+                respone_error_timeout: `${respone_error_timeout} ms`
+            })
+        }, respone_error_timeout) 
+    }
+    
 })
 
 
